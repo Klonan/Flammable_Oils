b98791c959a14634e6353e75a45a655a977d6cd9
Klonan <konranwoodhouse@hotmail.com>
2018-04-17 14:52:48 +0200

    Decleverize compatibilitize

--- control.lua
+++ control.lua
@@ -1,17 +1,85 @@
+local flammable_types
+
+init_flammable_types = function()
+  global.flammable_types =
+  {
+    ["crude-oil"] = true,
+    ["heavy-oil"] = true,
+    ["light-oil"] = true,
+    ["lubricant"] = true,
+    ["gas-hydrogen"] = true,
+    ["gas-methane"] = true,
+    ["gas-ethane"] = true,
+    ["gas-butane"] = true,
+    ["gas-propene"] = true,
+    ["liquid-naphtha"] = true,
+    ["liquid-mineral-oil"] = true,
+    ["liquid-fuel-oil"] = true,
+    ["gas-methanol"] = true,
+    ["gas-ethylene"] = true,
+    ["gas-benzene"] = true,
+    ["gas-synthesis"] = true,
+    ["gas-butadiene"] = true,
+    ["gas-phenol"] = true,
+    ["gas-ethylbenzene"] = true,
+    ["gas-styrene"] = true,
+    ["gas-formaldehyde"] = true,
+    ["gas-polyethylene"] = true,
+    ["gas-glycerol"] = true,
+    ["gas-natural-1"] = true,
+    ["liquid-multi-phase-oil"] = true,
+    ["gas-raw-1"] = true,
+    ["liquid-condensates"] = true,
+    ["liquid-ngl"] = true,
+    ["gas-chlor-methane"] = true,
+    ["hydrogen"] = true,
+    ["liquid-fuel"] = true,
+    ["diesel-fuel"] = true,
+    ["petroleum-gas"] = true
+  }
+  flammable_types = global.flammable_types
+end
+
+script.on_init(init_flammable_types)
+script.on_configuration_changed(function()
+  if not global.flammable_types then
+    init_flammable_types()
+  end
+  flammable_types = global.flammable_types
+end)
+
+script.on_load(function()
+  flammable_types = global.flammable_types
+end)
+
+remote.add_interface("flammable_oils", 
+{
+  add_flammable_type = function(name)
+    global.flammable_types[name] = true
+  end,
+  remove_flammable_type = function(name)
+    global.flammable_types[name] = nil
+  end,
+  get_flammable_types = function()
+    return global.flammable_types
+  end
+})
+
+
+
 script.on_event(defines.events.on_entity_died, function(event)
   local entity = event.entity
   local boxes = entity.fluidbox
-  local fluids = game.fluid_prototypes
   local num_pots = #boxes
   if num_pots == 0 then return end
+  local fluids = game.fluid_prototypes
   for k = 1, num_pots do
     local pot = boxes[k]
     if pot then 
-      if fluids[pot.name].fuel_value > 0 then
+      if flammable_types[pot.name] then
         local fraction = pot.amount/boxes.get_capacity(k)
         if fraction > 0.025 then 
-          flammable_explosion(entity, fraction)
-          return
+          return flammable_explosion(entity, fraction)
         end
       end
     end
@@ -36,7 +104,7 @@ function flammable_explosion(entity, fraction)
     for k = 1, math.ceil(width) do
       surface.create_entity{name = "oil-fire-flame", position = {pos.x+math.random(-radius,radius), pos.y+math.random(-radius,radius)}}
       for j = 1, math.ceil(4 * fraction) do
-        local burst = width+(2 * fraction)
+        local burst = width + (2 * fraction)
         surface.create_entity{name = "oil-fire-flame", position = {pos.x+math.random(-burst,burst), pos.y+math.random(-burst,burst)}}
       end
     end
